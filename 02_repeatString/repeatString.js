566065ad4a7d00669c6678d1b401cd1563cf490c
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2023-12-15 18:58:50 +0530

    solve repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = ''
+    if(num < 0) {
+        repeatedString = 'ERROR'
+    } else {
+        for (let i=0; i <num; i++){
+            repeatedString += string
+    }
+}
+    
+    
+    return repeatedString;
 };
+
+// repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
