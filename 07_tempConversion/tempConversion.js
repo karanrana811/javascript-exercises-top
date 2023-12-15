335510a3372aa7e489cdb082c844e5fbcd689333
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2023-12-15 22:07:38 +0530

    solve tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahren) {
+  let celsiusTemp = ((tempInFahren - 32)* (5/9));
+  return celsiusTemp % 1 === 0 ? +celsiusTemp : +celsiusTemp.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCels) {
+  let fahrenTemp = (((tempInCels * 9)/5) + 32);
+  return fahrenTemp % 1 === 0 ? +fahrenTemp : +fahrenTemp.toFixed(1);
 };
 
 // Do not edit below this line
