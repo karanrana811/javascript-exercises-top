d4458e966f62c7f8ae0832d4a16186e22358b021
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2023-12-15 20:56:48 +0530

    solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if (typeof numOne !== 'number' || typeof numTwo !== 'number' || numOne < 0 || numTwo < 0) {
+        return 'ERROR'
+    } else {
+        let sum = 0;
+        for (let i = (numOne > numTwo ? numTwo : numOne) ; i <= (numOne > numTwo ? numOne : numTwo); i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
