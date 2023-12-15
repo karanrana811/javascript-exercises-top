2a38f1f320e42baab7686c1c20765e3aaf19547d
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2023-12-15 19:24:20 +0530

    solve reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let realStr = string.toString();
+    let reversedString = ''
+    for (let i = (realStr.length - 1) ; i >= 0 ; i--){
+        reversedString += realStr[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
