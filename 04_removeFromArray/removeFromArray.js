39d4a1cd76377a12f8b8264384be2ed1c05818a7
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2023-12-15 20:18:18 +0530

    solve removeFromArray exercise using .filter and .includes array methods

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...rest) {
+// use filter method to filter out the input to be kicked out
+return inputArray.filter( (arrElement) => {
+        return !rest.includes(arrElement)
+    
+})
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
+
