3896e0ceba1c2c2b3b546eeb88d976dbbab77a7c
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-23 21:08:06 +0530

    solve all exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => acc + curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorialProd = 1;
+	if( a !== 0) {
+    for (let i = a ; i > 0 ; i--) {
+      factorialProd *= i
+    }
+  }
+  return factorialProd;
 };
 
 // Do not edit below this line
