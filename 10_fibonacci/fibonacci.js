685d42f7b399e22aa69d9ec38b5bb062c44acdf9
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-24 20:54:49 +0530

    solve exercise using for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibonacciArr = [];
+    for(let i = 0; i <= num ; i++){
+        if(i === 0){
+            fibonacciArr.push(1)
+        } else if(i === 1) {
+            fibonacciArr.push(1)
+        } else {
+            fibonacciArr.push(fibonacciArr[i - 1] + fibonacciArr[i-2])
+        }
 
+    }
+    return num < 0 ? 'OOPS': fibonacciArr[num - 1]
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
