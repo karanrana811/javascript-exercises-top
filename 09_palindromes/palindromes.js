03eac19b19d63f075d3106a2a40aa0f904bcf8bc
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-24 20:37:07 +0530

    solve exercise - learn about regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    // filter out punctuation using regex test, and join the string to one single string
+    const punctuationRegex = /[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/;
+    let filteredStr = str.split('').filter(char => !punctuationRegex.test(char)).join('').toLowerCase();
+    // reverse the joined punctuationless string and check if it's equal to the original str
+    return filteredStr === filteredStr.split('').reverse().join('')
+    
 };
 
 // Do not edit below this line
